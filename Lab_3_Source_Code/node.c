c1e8e53443829dd3c0eef33f755c3fbda1c999a4
Sandberg53 <128719484+Sandberg53@users.noreply.github.com>
2025-03-15 11:16:45 +0100

    Add files via upload

--- Lab_3_Source_Code/node.c
+++ Lab_3_Source_Code/node.c
@@ -1,57 +1,52 @@
 #include "node.h"
 
-#include <string.h>  // For memset
 
 void rtinit(struct distance_table *table, int node) {
+    sendPKT(table, node);
     printdt(table, node);
-
-    for (int neighbor = 0; neighbor < 4; neighbor++) { // Start from 0
-        if (is_neighbor(node, neighbor)) {
-            struct rtpkt pkt;
-            memset(&pkt, 0, sizeof(pkt)); // Ensure pkt is zeroed out
-
-            pkt.sourceid = node;
-            pkt.destid = neighbor;
-            for (int i = 0; i < 4; i++) {
-                pkt.mincost[i] = table->costs[i][node];  // Initialize mincosts
-            }
-
-            tolayer2(pkt);
-        }
-    }
 }
 
 
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
-    int src = pkt->sourceid;  // Neighbor sending the update
-    int updated = 0;          // Flag to check if changes occur
+    int src = node;
+    int via = pkt->sourceid; 
+    int updated = 0;         
 
-    for (int i = 0; i < 4; i++) {  // Loop through all nodes
-        int new_cost = table->costs[node][src] + pkt->mincost[i];  // Bellman-Ford update rule
-        if (new_cost < table->costs[i][node]) {  // Check if the new path is better
-            table->costs[i][node] = new_cost;
-            updated = 1;
+
+    for (int i = 0; i < 4; i++) {  
+        if (table->costs[via][i] > pkt->mincost[i]) {  
+            table->costs[via][i] = pkt->mincost[i];
+            updated = 1;            
         }
     }
 
-    printdt(table, node);  // Print table after update
-
-    // If table changed, send new routing updates to neighbors
     if (updated) {
-        struct rtpkt new_pkt;
-        new_pkt.sourceid = node;
+        sendPKT(table, node);
+    }
+        printdt(table, node); 
+}
 
-        for (int i = 0; i < 4; i++) {
-            new_pkt.mincost[i] = table->costs[i][node];  // Copy updated min costs
-        }
 
-        // Send to all neighbors
-        for (int dest = 0; dest < 4; dest++) {
-            if (is_neighbor(node, dest)) {
-                new_pkt.destid = dest;
-                tolayer2(new_pkt);
-            }
+void sendPKT(struct distance_table *table, int node){
+    int src = node;
+    struct rtpkt new_pkt;
+    new_pkt.sourceid = node;
+
+    for(int i = 0; i < 4; i++){
+        int temp = INF;
+        for(int j=0; j<4; j++){
+            int cost;
+            if(j == node){cost = table->costs[node][i];}
+            else{cost = table->costs[node][j] + table->costs[j][i];}
+
+            if(cost < temp){temp = cost;}
         }
-		
+        new_pkt.mincost[i] = temp;
+    }
+    for(int i=0; i<4; i++){
+        if(is_neighbor(src, i)){
+            new_pkt.destid = i;
+            tolayer2(new_pkt);
+        }
     }
 }
--- Lab_3_Source_Code/node.h
+++ Lab_3_Source_Code/node.h
@@ -3,9 +3,11 @@
 
 #include "sim_engine.h"
 #include <stdio.h>
+#include <string.h>
 
 void rtinit(struct distance_table *table, int nodeno);
 void rtupdate(struct distance_table *table, int nodeno, struct rtpkt *pkt);
 void printdt(struct distance_table *table, int nodeno);
+void sendPKT(struct distance_table *table, int node);
 
 #endif
